b8989ed22a1d6902da7c9700a70d2c8d6907a1ef
HuangStomach <nxmbest@qq.com>
2021-03-22 10:20:13 +0800

    671. 二叉树中第二小的节点

---
+++ 601-700/671.cpp
@@ -0,0 +1,35 @@
+/** 
+ * 671. 二叉树中第二小的节点
+ * 给定一个非空特殊的二叉树，每个节点都是正数，并且每个节点的子节点数量只能为 2 或 0。如果一个节点有两个子节点的话，那么该节点的值等于两个子节点中较小的一个。
+ * 更正式地说，root.val = min(root.left.val, root.right.val) 总成立。
+ * 给出这样的一个二叉树，你需要输出所有节点中的第二小的值。如果第二小的值不存在的话，输出 -1 。
+ */
+
+using namespace std;
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int findSecondMinimumValue(TreeNode *root) {
+        return find(root, root->val);
+    }
+
+    int find(TreeNode* node, int min) {
+        if (node == nullptr) return -1;
+        if (node->val > min) return node->val;
+        
+        int left = find(node->left, min);
+        int right = find(node->right, min);
+        if (left < 0) return right;
+        if (right < 0) return left;
+        return left > right ? right : left;
+    }
+};
