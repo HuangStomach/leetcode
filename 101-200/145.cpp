a0846e741c380296dc0f30f4ad2ad71de9e0e8fd
HuangStomach <nxmbest@qq.com>
2021-07-31 21:37:25 +0800

    145. 二叉树的后序遍历

---
+++ 101-200/145.cpp
@@ -0,0 +1,35 @@
+/**
+ * 145. 二叉树的后序遍历
+ * 给定一个二叉树，返回它的 后序 遍历。
+ */
+#include <vector>
+
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    void postorder(TreeNode *root, vector<int> &res) {
+        if (root == nullptr) {
+            return;
+        }
+        postorder(root->left, res);
+        postorder(root->right, res);
+        res.push_back(root->val);
+    }
+
+    vector<int> postorderTraversal(TreeNode *root) {
+        vector<int> res;
+        postorder(root, res);
+        return res;
+    }
+};
