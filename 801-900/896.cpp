b85f7ff04eb61b512e663f5baa44c411d4a2a2cc
HuangStomach <nxmbest@qq.com>
2021-08-04 21:38:39 +0800

    896. 单调数列

---
+++ 801-900/896.cpp
@@ -0,0 +1,26 @@
+/**
+ * 896. 单调数列
+ * 如果数组是单调递增或单调递减的，那么它是单调的。
+ * 如果对于所有 i <= j，A[i] <= A[j]，那么数组 A 是单调递增的。 如果对于所有 i <= j，A[i]> = A[j]，那么数组 A 是单调递减的。
+ * 当给定的数组 A 是单调数组时返回 true，否则返回 false。 
+ */
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) {
+        bool inc = true, dec = true;
+        int n = nums.size();
+        for (int i = 0; i < n - 1; ++i) {
+            if (nums[i] > nums[i + 1]) {
+                inc = false;
+            }
+            if (nums[i] < nums[i + 1]) {
+                dec = false;
+            }
+        }
+        return inc || dec;
+    }
+};
