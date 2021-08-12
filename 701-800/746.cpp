0293f9ceeefad4188b53136fd7f943f81e51dd22
HuangStomach <nxmbest@qq.com>
2021-08-12 11:02:06 +0800

    746. 使用最小花费爬楼梯

---
+++ 701-800/746.cpp
@@ -0,0 +1,26 @@
+/**
+ * 746. 使用最小花费爬楼梯
+ * 数组的每个下标作为一个阶梯，第 i 个阶梯对应着一个非负数的体力花费值 cost[i]（下标从 0 开始）。
+ * 每当你爬上一个阶梯你都要花费对应的体力值，一旦支付了相应的体力值，你就可以选择向上爬一个阶梯或者爬两个阶梯。
+ * 请你找出达到楼层顶部的最低花费。在开始时，你可以选择从下标为 0 或 1 的元素作为初始阶梯。
+ */
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        int prev = 0;
+        int curr = 0;
+
+        for (int i = 2; i <= n; i++) {
+            int next = min(curr + cost[i - 1], prev + cost[i - 2]);
+            prev = curr;
+            curr = next;
+        }
+
+        return curr;
+    }
+};
