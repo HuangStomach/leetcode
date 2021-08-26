a1b7a6ac814abce28bf8cb26c357dea4637dbd42
HuangStomach <nxmbest@qq.com>
2021-08-26 10:14:54 +0800

    867. 转置矩阵

---
+++ 801-900/867.cpp
@@ -0,0 +1,22 @@
+/**
+ * 867. 转置矩阵
+ * 给你一个二维整数数组 matrix， 返回 matrix 的 转置矩阵 。
+ * 矩阵的 转置 是指将矩阵的主对角线翻转，交换矩阵的行索引与列索引。 
+ */
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        int m = matrix.size(), n = matrix[0].size();
+        vector<vector<int>> transposed(n, vector<int>(m));
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                transposed[j][i] = matrix[i][j];
+            }
+        }
+        return transposed;
+    }
+};
