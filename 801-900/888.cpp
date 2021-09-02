f6a536977bfd99fe17b5ad1b7c8f3b0db261da99
HuangStomach <nxmbest@qq.com>
2021-09-02 10:45:53 +0800

    888. 公平的糖果棒交换

---
+++ 801-900/888.cpp
@@ -0,0 +1,32 @@
+/**
+ * 888. 公平的糖果棒交换
+ * 爱丽丝和鲍勃有不同大小的糖果棒：A[i] 是爱丽丝拥有的第 i 根糖果棒的大小，B[j] 是鲍勃拥有的第 j 根糖果棒的大小。
+ * 因为他们是朋友，所以他们想交换一根糖果棒，这样交换后，他们都有相同的糖果总量。（一个人拥有的糖果总量是他们拥有的糖果棒大小的总和。）
+ * 返回一个整数数组 ans，其中 ans[0] 是爱丽丝必须交换的糖果棒的大小，ans[1] 是 Bob 必须交换的糖果棒的大小。
+ * 如果有多个答案，你可以返回其中任何一个。保证答案存在。 
+ */
+#include <vector>
+#include <unordered_set>
+#include <numeric>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> fairCandySwap(vector<int>& aliceSizes, vector<int>& bobSizes) {
+        int sumA = accumulate(aliceSizes.begin(), aliceSizes.end(), 0);
+        int sumB = accumulate(bobSizes.begin(), bobSizes.end(), 0);
+        int delta = (sumA - sumB) / 2;
+
+        unordered_set<int> rec(aliceSizes.begin(), aliceSizes.end());
+        vector<int> ans;
+        for (auto& y : bobSizes) {
+            int x = y + delta;
+            if (rec.count(x)) {
+                ans = vector<int>{x, y};
+                break;
+            }
+        }
+        return ans;
+    }
+};
