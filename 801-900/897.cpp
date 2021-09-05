a1f3788050bfa0fa720fcf299d077fb546c0d501
HuangStomach <nxmbest@qq.com>
2021-09-05 19:24:56 +0800

    897. 递增顺序搜索树

---
+++ 801-900/897.cpp
@@ -0,0 +1,25 @@
+/**
+ * 897. 递增顺序搜索树
+ * 给你一棵二叉搜索树，请你 按中序遍历 将其重新排列为一棵递增顺序搜索树，使树中最左边的节点成为树的根节点，并且每个节点没有左子节点，只有一个右子节点。
+ */
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    TreeNode *increasingBST(TreeNode *node, TreeNode *ans = nullptr) {
+        if (node == nullptr)
+            return ans;
+        node->right = increasingBST(node->right, ans);
+        ans = increasingBST(node->left, node);
+        node->left = nullptr;
+        return ans;
+    }
+};
