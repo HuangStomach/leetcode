df9db22fcd6243660f9cfc7c9daca501041a7ddb
HuangStomach <nxmbest@qq.com>
2022-06-27 10:38:59 +0800

    922. 按奇偶排序数组 II

---
+++ 801-900/922.cpp
@@ -0,0 +1,26 @@
+/**
+ * 922. 按奇偶排序数组 II
+ * 给定一个非负整数数组 nums，  nums 中一半整数是 奇数 ，一半整数是 偶数 。
+ * 对数组进行排序，以便当 nums[i] 为奇数时，i 也是 奇数 ；当 nums[i] 为偶数时， i 也是 偶数 。
+ * 你可以返回 任何满足上述条件的数组作为答案 。
+ */
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> sortArrayByParityII(vector<int>& nums) {
+        int n = nums.size();
+        int j = 1;
+        for (int i = 0; i < n; i += 2) {
+            if (nums[i] % 2 == 1) {
+                while (nums[j] % 2 == 1) {
+                    j += 2;
+                }
+                swap(nums[i], nums[j]);
+            }
+        }   
+        return nums;
+    }
+};
