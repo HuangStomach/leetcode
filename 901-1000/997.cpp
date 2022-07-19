be10851c5f5ff26de2f4b4a9de1ef3ab30378e0f
HuangStomach <nxmbest@qq.com>
2022-07-19 11:58:59 +0800

    997. 找到小镇的法官

---
+++ 901-1000/997.cpp
@@ -0,0 +1,32 @@
+/**
+ * 997. 找到小镇的法官
+ * 小镇里有 n 个人，按从 1 到 n 的顺序编号。传言称，这些人中有一个暗地里是小镇法官。
+ * 如果小镇法官真的存在，那么：
+ * 小镇法官不会信任任何人。
+ * 每个人（除了小镇法官）都信任这位小镇法官。
+ * 只有一个人同时满足属性 1 和属性 2 。
+ * 给你一个数组 trust ，其中 trust[i] = [ai, bi] 表示编号为 ai 的人信任编号为 bi 的人。
+ * 如果小镇法官存在并且可以确定他的身份，请返回该法官的编号；否则，返回 -1 。
+ */
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<int> inDegrees(n + 1);
+        vector<int> outDegrees(n + 1);
+        for (auto& edge : trust) {
+            int x = edge[0], y = edge[1];
+            ++inDegrees[y];
+            ++outDegrees[x];
+        }
+        for (int i = 1; i <= n; ++i) {
+            if (inDegrees[i] == n - 1 && outDegrees[i] == 0) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
