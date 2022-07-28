e43457c58cbb6111d3521934e46aa8e057967138
HuangStomach <nxmbest@qq.com>
2022-07-28 22:07:30 +0800

    1030. 距离顺序排列矩阵单元格

---
+++ 1001-1100/1030.cpp
@@ -0,0 +1,36 @@
+/**
+ * 1030. 距离顺序排列矩阵单元格
+ * 给定四个整数 rows ,   cols ,  rCenter 和 cCenter 。有一个 rows x cols 的矩阵，你在单元格上的坐标是 (rCenter, cCenter) 。
+ * 返回矩阵中的所有单元格的坐标，并按与 (rCenter, cCenter) 的 距离 从最小到最大的顺序排。你可以按 任何 满足此条件的顺序返回答案。
+ * 单元格(r1, c1) 和 (r2, c2) 之间的距离为|r1 - r2| + |c1 - c2|。
+ */
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    const int dr[4] = {1, 1, -1, -1};
+    const int dc[4] = {1, -1, -1, 1};
+
+    vector<vector<int>> allCellsDistOrder(int rows, int cols, int rCenter, int cCenter) {
+        int maxDist = max(rCenter, rows - 1 - rCenter) + max(cCenter, cols - 1 - cCenter);
+        vector<vector<int>> ret;
+        int row = rCenter, col = cCenter;
+
+        ret.push_back({row, col});
+        for (int dist = 1; dist <= maxDist; dist++) {
+            row--;
+            for (int i = 0; i < 4; i++) {
+                while ((i % 2 == 0 && row != rCenter) || (i % 2 != 0 && col != cCenter)) {
+                    if (row >= 0 && row < rows && col >= 0 && col < cols) {
+                        ret.push_back({row, col});
+                    }
+                    row += dr[i];
+                    col += dc[i];
+                }
+            }
+        }
+        return ret;
+    }
+};
