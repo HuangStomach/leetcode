40aed3967c2f057aeccd4575d15def0f98ab892e
HuangStomach <nxmbest@qq.com>
2022-08-05 10:07:52 +0800

    1071. 字符串的最大公因子

---
+++ 1001-1100/1071.cpp
@@ -0,0 +1,17 @@
+/**
+ * 1071. 字符串的最大公因子
+ * 对于字符串 s 和 t，只有在 s = t + ... + t（t 自身连接 1 次或多次）时，我们才认定 “t 能除尽 s”。
+ * 给定两个字符串 str1 和 str2 。返回 最长字符串 x，要求满足 x 能除尽 str1 且 X 能除尽 str2 。
+ */
+#include <string>
+#include <numeric>
+
+using namespace std;
+
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        if (str1 + str2 != str2 + str1) return "";
+        return str1.substr(0, __gcd((int)str1.length(), (int)str2.length()));
+    }
+};
