497b339fc3b03f20067424802d3c5ffaf5f724f8
HuangStomach <nxmbest@qq.com>
2022-09-15 10:12:17 +0800

    373. 查找和最小的 K 对数字

---
+++ 301-400/373.cpp
@@ -0,0 +1,38 @@
+/**
+ * 373. 查找和最小的 K 对数字
+ * 给定两个以 升序排列 的整数数组 nums1 和 nums2 , 以及一个整数 k 。
+ * 定义一对值 (u,v)，其中第一个元素来自 nums1，第二个元素来自 nums2 。
+ * 请找到和最小的 k 个数对 (u1,v1),  (u2,v2)  ...  (uk,vk) 。
+ */
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+        auto cmp = [&nums1, &nums2](const pair<int, int> & a, const pair<int, int> & b) {
+            return nums1[a.first] + nums2[a.second] > nums1[b.first] + nums2[b.second];
+        };
+
+        int m = nums1.size();
+        int n = nums2.size();
+        vector<vector<int>> ans;   
+        priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(cmp)> pq(cmp);
+        for (int i = 0; i < min(k, m); i++) {
+            pq.emplace(i, 0);
+        }
+
+        while (k-- > 0 && !pq.empty()) {
+            auto [x, y] = pq.top(); 
+            pq.pop();
+            ans.emplace_back(initializer_list<int>{nums1[x], nums2[y]});
+            if (y + 1 < n) {
+                pq.emplace(x, y + 1);
+            }
+        }
+
+        return ans;
+    }
+};
