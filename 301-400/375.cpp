f366a98074e8e067384e0b7588d2b9e6fd1a2cc5
HuangStomach <nxmbest@qq.com>
2022-09-16 10:13:22 +0800

    375. 猜数字大小 II

--- 301-400/375.cpp
+++ 301-400/375.cpp
@@ -15,7 +15,7 @@ using namespace std;
 class Solution {
 public:
     int getMoneyAmount(int n) {
-        vector<vector<int>> f(n + 1, vector<int>(n + 1));
+        vector<vector<int>> f(n + 1, vector<int>(n + 1)); // 矩阵记录区间花费
         for (int i = n - 1; i >= 1; i--) {
             for (int j = i + 1; j <= n; j++) {
                 f[i][j] = j + f[i][j - 1];
