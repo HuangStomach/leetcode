b512c1141b54daf7e75ce27553a1a605256b28c3
HuangStomach <nxmbest@qq.com>
2023-03-13 11:58:28 +0800

    382. 链表随机节点

---
+++ 301-400/382.cpp
@@ -0,0 +1,37 @@
+/**
+ * 382. 链表随机节点
+ * 给你一个单链表，随机选择链表的一个节点，并返回相应的节点值。每个节点 被选中的概率一样 。
+ * 实现 Solution 类：
+ * Solution(ListNode head) 使用整数数组初始化对象。
+ * int getRandom() 从链表中随机选择一个节点并返回该节点的值。链表中所有节点被选中的概率相等。
+ */
+#include <random>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+    ListNode *head;
+public:
+    Solution(ListNode *head) {
+        this->head = head;
+    }
+
+    int getRandom() {
+        int i = 1, ans = 0;
+        for (auto node = head; node; node = node->next) {
+            if (rand() % i == 0) { // 1/i 的概率选中（替换为答案）
+                ans = node->val;
+            }
+            ++i;
+        }
+        return ans;
+    }
+};
