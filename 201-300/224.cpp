09ca5e301d025b5b2b6408e5c9f2e5a3f2a40dfe
HuangStomach <nxmbest@qq.com>
2023-08-19 21:21:27 +0800

    224. 基本计算器

---
+++ 201-300/224.cpp
@@ -0,0 +1,47 @@
+/**
+ * 224. 基本计算器
+ * 给你一个字符串表达式 s ，请你实现一个基本计算器来计算并返回它的值。
+ * 注意:不允许使用任何将字符串作为数学表达式计算的内置函数，比如 eval() 。
+ */
+#include <string>
+#include <stack>
+
+using namespace std;
+
+class Solution {
+public:
+    int calculate(string s) {
+        stack<int> ops;
+        ops.push(1);
+        int sign = 1;
+
+        int ret = 0;
+        int n = s.length();
+        int i = 0;
+        while (i < n) {
+            if (s[i] == ' ') i++;
+            else if (s[i] == '+') {
+                sign = ops.top();
+                i++;
+            } else if (s[i] == '-') {
+                sign = -ops.top();
+                i++;
+            } else if (s[i] == '(') {
+                ops.push(sign);
+                i++;
+            } else if (s[i] == ')') {
+                ops.pop();
+                i++;
+            } 
+            else {
+                long num = 0;
+                while (i < n && s[i] >= '0' && s[i] <= '9') {
+                    num = num * 10 + s[i] - '0';
+                    i++;
+                }
+                ret += sign * num;
+            }
+        }
+        return ret;
+    }
+};
