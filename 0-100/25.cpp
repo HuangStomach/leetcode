8388ee47cb48a1b6af95f10a65aa1d0edfc5dc78
HuangStomach <nxmbest@qq.com>
2023-08-20 09:14:51 +0800

    25. K 个一组翻转链表

---
+++ 0-100/25.cpp
@@ -0,0 +1,61 @@
+/**
+ * 25. K 个一组翻转链表
+ * 给你链表的头节点 head ，每 k 个节点一组进行翻转，请你返回修改后的链表。
+ * k 是一个正整数，它的值小于或等于链表的长度。如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序。
+ * 你不能只是单纯的改变节点内部的值，而是需要实际进行节点交换。
+ */
+#include <utility>
+
+using namespace std;
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    pair<ListNode*, ListNode*> myReverse(ListNode* head, ListNode* tail) {
+        ListNode* prev = tail->next;
+        ListNode* p = head;
+        while (prev != tail) {
+            ListNode* nex = p->next;
+            p->next = prev;
+            prev = p;
+            p = nex;
+        }
+        return {tail, head};
+    }
+
+    ListNode *reverseKGroup(ListNode *head, int k) {
+        ListNode* hair = new ListNode(0);
+        hair->next = head;
+        ListNode* pre = hair;
+
+        while (head) {
+            ListNode* tail = pre;
+            // 查看剩余部分长度是否大于等于 k
+            for (int i = 0; i < k; ++i) {
+                tail = tail->next;
+                if (!tail) return hair->next;
+            }
+            ListNode* nex = tail->next;
+
+            pair<ListNode*, ListNode*> result = myReverse(head, tail);
+            head = result.first;
+            tail = result.second;
+
+            // 把子链表重新接回原链表
+            pre->next = head;
+            tail->next = nex;
+            pre = tail;
+            head = tail->next;
+        }
+
+        return hair->next;
+    }
+};
