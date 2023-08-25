83326fec711f8d010fb0c9b10f3b5c170515e2b9
HuangStomach <nxmbest@qq.com>
2023-08-25 09:40:57 +0800

    918. 环形子数组的最大和

---
+++ 901-1000/918.cpp
@@ -0,0 +1,32 @@
+/**
+ * 918. 环形子数组的最大和
+ * 给定一个长度为 n 的环形整数数组 nums ，返回 nums 的非空 子数组 的最大可能和 。
+ * 环形数组 意味着数组的末端将会与开头相连呈环状。形式上， nums[i] 的下一个元素是 nums[(i + 1) % n] ， nums[i] 的前一个元素是 nums[(i - 1 + n) % n] 。
+ * 子数组 最多只能包含固定缓冲区 nums 中的每个元素一次。形式上，对于子数组 nums[i], nums[i + 1], ..., nums[j] ，不存在 i <= k1, k2 <= j 其中 k1 % n == k2 % n 。
+ */
+#include <vector>
+#include <deque>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxSubarraySumCircular(vector<int>& nums) {
+        int n = nums.size();
+        deque<pair<int, int>> q;
+        int pre = nums[0], res = nums[0];
+        q.push_back({0, pre});
+        for (int i = 1; i < 2 * n; i++) {
+            while (!q.empty() && q.front().first < i - n) {
+                q.pop_front();
+            }
+            pre += nums[i % n];
+            res = max(res, pre - q.front().second);
+            while (!q.empty() && q.back().second >= pre) {
+                q.pop_back();
+            }
+            q.push_back({i, pre});
+        }
+        return res;
+    }
+};
