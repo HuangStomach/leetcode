549934150931c58faeebbb6835966b540d418604
HuangStomach <nxmbest@qq.com>
2023-08-28 10:46:41 +0800

    149. 直线上最多的点数

---
+++ 101-200/149.cpp
@@ -0,0 +1,48 @@
+/**
+ * 149. 直线上最多的点数
+ * 给你一个数组 points ，其中 points[i] = [xi, yi] 表示 X-Y 平面上的一个点。求最多有多少个点在同一条直线上。
+ */
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    int gcd(int a, int b) {
+        return b ? gcd(b, a % b) : a;
+    }
+
+    int maxPoints(vector<vector<int>>& points) {
+        int n = points.size();
+        if (n <= 2) return n;
+
+        int ret = 0;
+        for (int i = 0; i < n; i++) {
+            if (ret >= n - i || ret > n / 2) break;
+
+            unordered_map<int, int> mp;
+            for (int j = i + 1; j < n; j++) {
+                int x = points[i][0] - points[j][0];
+                int y = points[i][1] - points[j][1];
+                if (x == 0)  y = 1;
+                else if (y == 0)  x = 1;
+                else {
+                    if (y < 0) {
+                        x = -x;
+                        y = -y;
+                    }
+                    int gcdXY = gcd(abs(x), abs(y));
+                    x /= gcdXY, y /= gcdXY;
+                }
+                mp[y + x * 20001]++;
+            }
+            int maxn = 0;
+            for (auto& [_, num] : mp) {
+                maxn = max(maxn, num + 1);
+            }
+            ret = max(ret, maxn);
+        }
+        return ret;
+    }
+};
