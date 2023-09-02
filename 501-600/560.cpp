3c0725cba382e94c47cc8f054247762c1ac7a8d8
HuangStomach <nxmbest@qq.com>
2023-09-02 14:19:53 +0800

    560. 和为 K 的子数组

---
+++ 501-600/560.cpp
@@ -0,0 +1,26 @@
+/**
+ * 560. 和为 K 的子数组
+ * 给你一个整数数组 nums 和一个整数 k ，请你统计并返回 该数组中和为 k 的连续子数组的个数 。
+ */
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> mp;
+        mp[0] = 1;
+        int count = 0;
+        int pre = 0;
+        for (auto& x:nums) {
+            pre += x;
+            if (mp.find(pre - k) != mp.end()) {
+                count += mp[pre - k];
+            }
+            mp[pre]++;
+        }
+        return count;
+    }
+};
