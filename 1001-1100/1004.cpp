8f406a04fa2fbd3a484ea2c7399c93da3c202935
HuangStomach <nxmbest@qq.com>
2023-10-06 10:56:44 +0800

    1004. 最大连续1的个数 III

---
+++ 1001-1100/1004.cpp
@@ -0,0 +1,27 @@
+/**
+ * 1004. 最大连续1的个数 III
+ * 给定一个二进制数组 nums 和一个整数 k，如果可以翻转最多 k 个 0 ，则返回 数组中连续 1 的最大个数 。
+ */
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int res = 0;
+
+        int left = 0;
+        int right = 0;
+        int zero = 0;
+
+        for(int right = 0; right < nums.size(); right++){
+            if (nums[right] == 0) ++zero;
+            while (zero > k) {
+                if (nums[left++] == 0) --zero;
+            }
+            res = max(res, right - left + 1);
+        }
+        return res;
+    }
+};
